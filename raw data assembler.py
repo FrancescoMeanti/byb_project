6857901080b94a4e25bae10af86fd993cd83d723
FrancescoMeanti <f.meanti@lancaster.ac.uk>
2025-03-28 09:52:15 +0000

    Add files via upload

---
+++ raw data assembler.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import scanpy as sc
+
+
+adata = sc.read_mtx(r"data\raw data\RNA_counts.mtx")
+adata_bc=pd.read_csv(r"data\raw data\cells.csv",header=0)
+adata_features=pd.read_csv(r"data\raw data\genes.csv",header=0)
+adata= adata.T
+adata.obs['cell_id']= adata_bc['cell'].tolist()
+adata.var['gene_name']= adata_features['gene'].tolist()
+adata.var_names = adata_features['gene'].tolist()
+#adata.var.index= adata.var['gene_name']
+sample_obs = pd.read_csv(r"data\raw data\Combined_Metadata.csv",header=0)
+adata.obs = sample_obs
+adata.raw = adata.copy()
+ 
+ 
+#adata.var_names_make_unique()
+immune_data = adata
+
+immune_data.write_h5ad(r"data\H5AD Objects\Immune_Raw_New.h5ad")
